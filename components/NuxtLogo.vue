952c7522394c25430ca8c9bbee477eb8356be5cc
miracle <liegeubani@gmail.com>
2022-07-12 13:07:40 +0100

    nuxtjs set up

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav>
+    <div>
+      <nuxt-link to="/">
+        <nuxt-logo />
+      </nuxt-link>
+    </div>
+    <div></div>
+    </nav>
+  </div>
+</template>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
